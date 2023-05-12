a1f5bc97aa2804381de17c90c888f23e2bf1ecad
itsfinn <39669621+itsfinn@users.noreply.github.com>
2023-05-12 11:30:14 +0800

    Update strings.go

--- pkg/strings/strings.go
+++ pkg/strings/strings.go
@@ -7,3 +7,30 @@ func FirstN(s string, n int) string {
 	}
 	return s
 }
+
+// RepeatJoin repeats the string s count times and joins them with seq.
+// It returns an empty string if count is negative or zero.
+// It panics if the result is longer than the maximum string length.
+// It is a combination of strings.Repeat and strings.Join functions.
+func RepeatJoin(s string, count int, seq string) string {
+	
+	// Check if count is negative or zero
+	if count <= 0 {
+		return ""
+	}
+	// Check if s is empty
+	if s == "" {
+		return strings.Repeat(seq, count-1)
+	}
+	// Create a buffer to write the result
+	var buf strings.Builder
+	// Write s and seq count-1 times
+	for i := 0; i < count-1; i++ {
+		buf.WriteString(s)
+		buf.WriteString(seq)
+	}
+	// Write s one more time
+	buf.WriteString(s)
+	// Return the buffer as a string
+	return buf.String()
+}
